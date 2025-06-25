c00eae32cb6a00f4e1cec253c680d95fc4fad6e3
Mohamed Mushraf <mushrafmim@gmail.com>
2025-06-25 22:56:51 +0530

    feat: navigate select through keyboard

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -1,5 +1,5 @@
 import {SearchIcon, X} from "lucide-react";
-import {useRef, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {SearchLocationType} from "@/types/SearchLocationType";
 import {searchLocations} from "@/requests";
 import {useCurrentWeather} from "@/context/CurrentWeatherContext";
@@ -8,6 +8,7 @@ import {IconLocation} from "@tabler/icons-react";
 export default function LocationSearch() {
     const [searchKey, setSearchKey] = useState("Colombo, Sri Lanka")
     const [searchLocationResults, setSearchLocationResults] = useState<SearchLocationType[]>([]);
+    const [activeIndex, setActiveIndex] = useState(0);
 
     const { currentLocation, setCurrentLocation } = useCurrentWeather();
 
@@ -57,6 +58,32 @@ export default function LocationSearch() {
         locationSearchRef.current?.focus();
     }
 
+    // on enter key press, select the active location
+    const handleKeyDown = (e: KeyboardEvent) => {
+        if (e.key === 'Enter' && searchLocationResults.length > 0) {
+            const selectedLocation = searchLocationResults[activeIndex];
+            setCurrentLocation(selectedLocation);
+            setSearchKey(`${selectedLocation.name}, ${selectedLocation.country}`);
+            setSearchLocationResults([]);
+        } else if (e.key === 'ArrowDown') {
+            setActiveIndex((prevIndex) => Math.min(prevIndex + 1, searchLocationResults.length - 1));
+        } else if (e.key === 'ArrowUp') {
+            setActiveIndex((prevIndex) => Math.max(prevIndex - 1, 0));
+        }
+    };
+
+    useEffect(() => {
+        const inputElement = locationSearchRef.current;
+        if (inputElement) {
+            inputElement.addEventListener('keydown', handleKeyDown);
+        }
+        return () => {
+            if (inputElement) {
+                inputElement.removeEventListener('keydown', handleKeyDown);
+            }
+        };
+    }, [searchLocationResults, activeIndex]);
+
     return (
         <div>
             <div className="flex gap-2 items-center bg-white/40 rounded-lg shadow-sm p-2 relative">
@@ -80,14 +107,16 @@ export default function LocationSearch() {
                 {searchLocationResults.length > 0 && (
                     <div className="absolute bg-white shadow-lg rounded-lg mt-2 z-10 left-0 right-0">
                         <ul>
-                            {searchLocationResults.map((location) => (
+                            {searchLocationResults.map((location, i) => (
                                 <li
+                                    onMouseEnter={() => setActiveIndex(i)}
                                     key={location.id}
-                                    className="cursor-pointer hover:bg-gray-100 p-2"
+                                    className={`cursor-pointer overflow-hidden hover:bg-gray-100 p-2 ${activeIndex === i ? 'bg-gray-200' : ''}`}
                                     onClick={() => {
                                         setCurrentLocation(location);
                                         setSearchKey(`${location.name}, ${location.country}`);
                                         setSearchLocationResults([]);
+                                        setActiveIndex(0);
                                     }}
                                 >
                                     <strong>{location.name}</strong>, {location.country}
